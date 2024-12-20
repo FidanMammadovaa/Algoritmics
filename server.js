0f244555374a90ae6046f3b2adf3b2ed2a6c83b1
Fidan Mammadova <gold_wolverine1@mail.ru>
2024-12-20 17:54:33 +0400

    Done

--- server.js
+++ server.js
@@ -1,46 +1,20 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const port = 3000;
 
-let products = [];
+const myName = 'Kamal'
 
-function generateRandomProducts() {
-    const names = ["Apple", "Banana", "milk", "cocao", "bread", "book", "copybook", "pen", "pencil", "notebook"];
-    for (let i = 0; i < 10; i++) {
-        let product = {
-            id: i + 1,
-            name: names[i]
-        }
-        products.push(product);
-    }
-}
+app.get('/', (req, res) =>
+    res.send(`Menim adim: ${myName}`));
 
-generateRandomProducts();
+app.get('/card', (req, res) =>
+    res.send(`Bu route kartlar üçün cavabdehdir`));
 
-app.get('/products', (req, res) => {
-    const limit = parseInt(req.query.limit) || 5;
-    const offset = parseInt(req.query.offset) || 0;
 
-    const paginatedProducts = products.slice(offset, offset + limit);
+app.get('/client', (req, res) =>
+    res.send(`Bu marşrut müştərilər üçün cavabdehdir`));
 
-    res.json({
-        data: paginatedProducts,
-        total: products.length,
-        limit: limit,
-        offset: offset
-    });
-});
-app.get('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const product = products.find(p => p.id === productId);
+const POST = 3000;
 
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).send('Product not found');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(POST, ()=>{
+    console.log(`http://localhost:${POST}`);
+})
